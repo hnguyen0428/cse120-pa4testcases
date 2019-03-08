9643664924f21efbb01689ee7299e5066e315e8d
Hoang Nguyen <hnguyen0428@yahoo.com>
2019-03-07 20:47:58 -0800

    automatic tests

--- README.md
+++ README.md
@@ -10,3 +10,15 @@ To run:
     ./runTests -t <test case> [-e]
     
 Running with -e will run the test case using the reference solution.
+
+To run all the tests, run
+
+    ./runAll.sh
+    
+This will automatically diff all your output vs. the reference solution
+output.
+
+Note: You must change all the error messages so that it does not contain
+"My" in order for the output to match the reference solution. For example,
+in MyYieldThread, make sure that it says "YieldThread" in the print statement
+instead.
--- runAll.sh
+++ runAll.sh
@@ -8,9 +8,24 @@ tests=(1 2 3 4 5 6 7 8 9 10 11 12 13 14)
 for i in "${tests[@]}"
 do
   ./runTests -t $i > "$i.OUTPUT"
+  sed '1d' "$i.OUTPUT" > "tempfile"
+  mv "tempfile" "$i.OUTPUT"
 done
 
 for i in "${tests[@]}"
 do
   ./runTests -t $i -e > "$i.EXPECTED"
+  sed '1d' "$i.EXPECTED" > "tempfile"
+  mv "tempfile" "$i.EXPECTED"
+done
+
+for i in "${tests[@]}"
+do
+  DIFF=$(diff "$i.OUTPUT" "$i.EXPECTED")
+  if [ "$DIFF" == "" ]
+  then
+    echo "Test $i: PASSED"
+  else
+    echo "Test $i: FAILED"
+  fi
 done
