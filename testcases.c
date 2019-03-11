1bc08f46df933588209aee1f28568bdca6ffd4d8
Hoang Nguyen <hnguyen0428@yahoo.com>
2019-03-10 17:30:00 -0700

    woops for got to add test21

--- testcases.c
+++ testcases.c
@@ -501,10 +501,10 @@ int checkZeros(char arr[], int size) {
 }
 
 
-#define NUMTESTS 20
+#define NUMTESTS 21
 void (*tests[])() = {test1, test2, test3, test4, test5, test6, test7, test8, 
 test9, test10, test11, test12, test13, test14, test15, test16, test17, test18,
-test19, test20};
+test19, test20, test21};
 
 void usage(char* argv[]) {
   Printf(
